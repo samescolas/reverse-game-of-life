877d0fe3cdcf94f01cac112b7448e99a21fe6b2d
samescolas <samescolas@gmail.com>
2017-07-14 20:02:08 -0700

    Creating gene checking functions

--- ga/FitnessChecks.py
+++ ga/FitnessChecks.py
@@ -1,39 +1,37 @@
+import sys
+
 def	check_pop(gene, val):
-	print 'checking pop of ' + gene + '...'
+	if '1' in gene:
+		bucket = gene.index('1')
+	else:
+		bucket = 0
+	print 'checking gene ' + gene + ' to see if ' + str(val['population']) + ' is within ' + str(bucket) + '...'
+	sys.exit(1)
 	return True
 
 def	check_alive(gene, val):
-	print 'checking if gene ' + gene + ' is alive...'
 	return True
 
 def	check_neighbors(gene, val):
-	print 'checking how many neighbors ' + gene + ' has...'
 	return True
 
 def	check_living_neighbors(gene, val):
-	print 'checking how many living neighbors ' + gene + 'has...'
 	return True
 
 def	check_size(gene, val):
-	print 'checking the size of ' + gene + '...'
 	return True
 
 def	check_isolation_layers(gene, val):
-	print 'checking isolation layers of ' + gene + '...'
 	return True
 
 def	check_nearest_edge(gene, val):
-	print 'checking nearest edge to ' + gene + '...'
 	return True
 
 def	check_quadrant(gene, val):
-	print 'checking ' + gene + '\'s quadrant...'
 	return True
 
 def	check_quadrant_density(gene, val):
-	print 'checking dentisy of ' + gene + '\'s quadrant...'
 	return True
 
 def	check_delta(gene, val):
-	print 'checking delta of ' + gene + '...'
 	return True
