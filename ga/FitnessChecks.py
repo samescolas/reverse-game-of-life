e2af19cd566f2869c0275f25be982c287e8d6307
samescolas <samescolas@gmail.com>
2017-07-14 18:37:18 -0700

    Functions to check fitnes of individual genes within chromosome.

---
+++ ga/FitnessChecks.py
@@ -0,0 +1,39 @@
+def	check_pop(gene, val):
+	print 'checking pop of ' + gene + '...'
+	return True
+
+def	check_alive(gene, val):
+	print 'checking if gene ' + gene + ' is alive...'
+	return True
+
+def	check_neighbors(gene, val):
+	print 'checking how many neighbors ' + gene + ' has...'
+	return True
+
+def	check_living_neighbors(gene, val):
+	print 'checking how many living neighbors ' + gene + 'has...'
+	return True
+
+def	check_size(gene, val):
+	print 'checking the size of ' + gene + '...'
+	return True
+
+def	check_isolation_layers(gene, val):
+	print 'checking isolation layers of ' + gene + '...'
+	return True
+
+def	check_nearest_edge(gene, val):
+	print 'checking nearest edge to ' + gene + '...'
+	return True
+
+def	check_quadrant(gene, val):
+	print 'checking ' + gene + '\'s quadrant...'
+	return True
+
+def	check_quadrant_density(gene, val):
+	print 'checking dentisy of ' + gene + '\'s quadrant...'
+	return True
+
+def	check_delta(gene, val):
+	print 'checking delta of ' + gene + '...'
+	return True
