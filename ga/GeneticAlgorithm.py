dc7e50ba8336ea506ac7dfb5cb4f54c4603a4543
Sam Escolas <sescolas@student.42.us.org>
2017-07-16 03:03:40 -0400

    Modifying chromosome encoding

--- ga/GeneticAlgorithm.py
+++ ga/GeneticAlgorithm.py
@@ -1,23 +1,23 @@
 import random
 import FitnessCalc
 
-# {00}[01,06) | population
-# {06}[07,08) | alive?
-# {08}[09,12) | corner,side,center
-# {12}[13,19) | # of living neighbors
-# {19}[20,25) | # of living cells
-# {25}[26,31) | layers of isolation
-# {31}[32,35) | distance to nearest edge
-# {35}[36,40) | quadrant
-# {40}[41,46) | quadrant population density
-# {46}[47,52) | delta
-# {52}[53,54) | prediction
+# {00}[01,05) | population
+# {05}[06,07) | alive?
+# {07}[08,10) | corner,side,center
+# {10}[11,15) | # of living neighbors BINARY
+# {17}[18,23) | # of living cells
+# {23}[24,29) | layers of isolation
+# {29}[30,33) | distance to nearest edge
+# {33}[34,38) | quadrant
+# {38}[39,44) | quadrant population density
+# {44}[45,50) | delta
+# {50}[51,52) | prediction
 
 class GeneticAlgorithm:	
 	def __init__(self):
 		self.genes = [
-			(0,5), (6,1), (8,3), (12,6), (19,5),
-			(25,5), (31,3), (35,4), (40,5), (46,5), (52,1)
+			(0,4), (5,1), (7,2), (10,4), (15,5),
+			(21,5), (27,3), (31,4), (36,5), (42,5), (48,1)
 		]
 		self.chromosome_length = 53
 		self.crossover_rate = 0.7
@@ -40,7 +40,7 @@ class GeneticAlgorithm:
 				chromosome[activator] = '1'
                         if random.random() > float(1.0/(length+1)):
 			    chromosome[activator + random.randint(0, length)] = '1'
-		if random.random() < self.activation_rate:
+                if random.random() < 0.5:
 			chromosome += '1'
 		else:
 			chromosome += '0'
