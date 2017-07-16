853f4fbadfd46b3847e595ea99bee749d7d2c67d
Sam Escolas <sescolas@student.42.us.org>
2017-07-16 02:26:44 -0400

    Reworking fitness function

--- ga/fitness_checks.py
+++ ga/fitness_checks.py
@@ -1,7 +1,8 @@
 import sys
 
 # 5 buckets and 400 total == 80
-def	check_pop(gene, val):
+def	check_pop(gene, val, cell):
+        print 'pop: ' + gene
 	if '1' in gene:
 		bucket = gene.index('1')
 	else:
@@ -10,29 +11,50 @@ def	check_pop(gene, val):
             return True
 	return False
 
-def	check_alive(gene, val):
+def	check_alive(gene, val, cell):
+        if val['start.' + str(cell)] == gene:
+            return True
+	return False
+
+def	check_neighbors(gene, val, cell):
+        print 'neighbors: ' + gene
+        if '1' not in gene:
+            return False
+        if cell == 1 or cell == 20 or cell == 381 or cell == 400:
+            if gene.index('1') == 3:
+                return True
+        elif cell % 20 < 2:
+            if gene.index('1') == 2:
+                return True
+        elif gene.index('1') == 3:
+            return True
+	return False
+
+def	check_living_neighbors(gene, val, cell):
+        print 'living neighbors: '  + gene
+        if cell == 1 or cell == 20 or cell == 381 or cell == 400:
+            neighbors = 3
+        elif cell % 20 < 2:
+            neighbors = 5
+        else:
+            neighbors = 8
+        
 	return True
 
-def	check_neighbors(gene, val):
+def	check_size(gene, val, cell):
 	return True
 
-def	check_living_neighbors(gene, val):
+def	check_isolation_layers(gene, val, cell):
 	return True
 
-def	check_size(gene, val):
+def	check_nearest_edge(gene, val, cell):
 	return True
 
-def	check_isolation_layers(gene, val):
+def	check_quadrant(gene, val, cell):
 	return True
 
-def	check_nearest_edge(gene, val):
+def	check_quadrant_density(gene, val, cell):
 	return True
 
-def	check_quadrant(gene, val):
-	return True
-
-def	check_quadrant_density(gene, val):
-	return True
-
-def	check_delta(gene, val):
+def	check_delta(gene, val, cell):
 	return True
