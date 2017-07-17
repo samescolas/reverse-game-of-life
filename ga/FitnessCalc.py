7ababc673f5efd51a194e4f4a2ade165d733ca07
samescolas <samescolas@gmail.com>
2017-07-17 13:46:45 -0700

    Almost done with these changes..

--- ga/FitnessCalc.py
+++ ga/FitnessCalc.py
@@ -6,7 +6,7 @@ class FitnessCalc:
 	def __init__(self, training_data):
 		self.genes = [
 			(0,4), (5,1), (7,2), (10,4), (15,5),
-			(21,5), (27,3), (31,4), (36,5), (42,5), (48,1)
+			(21,5), (27,3), (31,3), (35,5), (41,5), (47,1)
 		]
 		self.chromosome_length = 53
 		self.training_data = training_data
@@ -16,8 +16,7 @@ class FitnessCalc:
 	def calculate_fitness(self,chromosome):
 		results = 0,0,0,0
 		for ix in xrange(1,self.num_examples + 1):
-			print self.training_data[ix]
-			#results = [sum(x) for x in zip(results, self.test_chromosome(self.training_data[ix], chromosome))]
+			results = [sum(x) for x in zip(results, self.test_chromosome(self.training_data[ix], chromosome))]
 		print 'tp: {}'.format(results[0])
 		print 'fp: {}'.format(results[1])
 		print 'tn: {}'.format(results[2])
@@ -59,22 +58,21 @@ class FitnessCalc:
 
         # test chromosome against each cell in grid
 	def test_gene(self, gene, data, gene_id):
-            test = []
-            if gene_id == 0:
-                test = [check_pop(gene, data['population'])]*400
-			elif gene_id == 9:
-				test = [check_delta(gene, data['delta'])]*400
-			#elif ix == 9 then do delta once instead of 400 times
-            else:
-                for cell in xrange(1,401):
-                    test.append( {
-                        1: check_alive,
-                        2: check_neighbors,
-                        3: check_living_neighbors,
-                        4: check_size,
-                        5: check_isolation_layers,
-                        6: check_nearest_edge,
-                        7: check_quadrant,
-                        8: check_quadrant_density,
-                    }[gene_id](gene, data['cells'][ix], cell) )
-            return test
+		test = []
+		if gene_id == 0:
+			test = [check_pop(gene, data['population'])]*400
+		elif gene_id == 9:
+			test = [check_delta(gene, data['delta'])]*400
+		else:
+			for cell in xrange(1,401):
+				test.append( {
+					1: check_alive,
+					2: check_neighbors,
+					3: check_living_neighbors,
+					4: check_size,
+					5: check_isolation_layers,
+					6: check_nearest_edge,
+					7: check_quadrant,
+					8: check_quadrant_density,
+				}[gene_id](gene, data['cells'][ix], cell) )
+		return test
