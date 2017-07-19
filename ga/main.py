9f7c8432d91c987e10bd44c3b5c6cb1c509506ef
samescolas <samescolas@gmail.com>
2017-07-18 21:45:17 -0700

    How many generations is too many generations...?

--- ga/main.py
+++ ga/main.py
@@ -19,30 +19,32 @@ with open('../resources/train.csv', 'rb') as csvfile:
 		if i == 0:
 			keys = record
 			continue
-		elif i == 1001:
+		elif i < 5000:
+			continue
+		elif i == 5501:
 			break
-                summary[i] = {
-					'population': reduce((lambda x,y: int(x)+int(y)), record[402:]),
-					'delta': record[1],
-					'cells': {}
-				}
-                print str(i/1000.0*100) + '%'
-                for j,cell in enumerate(keys[402:]):
-					neighbors = get_neighbors(int(cell[5:]), record[401:])
-					density = get_quadrant_densities(record[401:])
-					summary[i]['cells'][j+1] = {
-						'status': record[j+2],
-						'outcome': record[j+398],
-						'neighbors': len(neighbors[0]),
-						'living_neighbors': len(neighbors[1]),
-						'quadrant': get_quadrant(int(cell[5:])),
-						'quadrant_density': density[get_quadrant(int(cell[5:])) - 1],
-						'nearest_edge': get_nearest_edge(int(cell[5:]))
-					}
-					if record[j+2] == '1':
-						summary[i]['cells'][j+1]['size'] = get_size(int(cell[5:]), record[401:])
-					else:
-						summary[i]['cells'][j+1]['size'] = 0
+		summary[i-4999] = {
+			'population': reduce((lambda x,y: int(x)+int(y)), record[402:]),
+			'delta': record[1],
+			'cells': {}
+		}
+		print str(i)
+		for j,cell in enumerate(keys[402:]):
+			neighbors = get_neighbors(int(cell[5:]), record[401:])
+			density = get_quadrant_densities(record[401:])
+			summary[i-4999]['cells'][j+1] = {
+				'status': record[j+2],
+				'outcome': record[j+398],
+				'neighbors': len(neighbors[0]),
+				'living_neighbors': len(neighbors[1]),
+				'quadrant': get_quadrant(int(cell[5:])),
+				'quadrant_density': density[get_quadrant(int(cell[5:])) - 1],
+				'nearest_edge': get_nearest_edge(int(cell[5:]))
+			}
+			if record[j+2] == '1':
+				summary[i-4999]['cells'][j+1]['size'] = get_size(int(cell[5:]), record[401:])
+			else:
+				summary[i-4999]['cells'][j+1]['size'] = 0
 
 # create GA instance
 ga = GeneticAlgorithm()
@@ -53,7 +55,7 @@ fc = FitnessCalc(summary)
 #create initial population
 ga.create_initial_population()
 
-for generations in xrange(1,11):
+for generations in xrange(1,21):
 	max_fitness = 0.0
 	print 'generation ' + str(generations)
 	print 'max fitness: ' + str(max_fitness)
@@ -91,5 +93,5 @@ for indiv in ga.population:
 		winner = indiv
 
 print 'best chromosome: ' + winner['chromosome']
+winner['fitness'] = fc.calculate_fitness(winner['chromosome'])
 print 'fitness: ' + str(winner['fitness'])
-fc.calculate_fitness(winner['chromosome'])
