e99267acf5576aeb9eb302ae51b7121a15a208b7
samescolas <samescolas@gmail.com>
2017-07-19 21:02:47 -0700

    Still playing around with things.

--- ga/main.py
+++ ga/main.py
@@ -1,48 +1,11 @@
 #!/usr/bin/python
 
-import csv
 from FitnessCalc import FitnessCalc
 from GeneticAlgorithm import GeneticAlgorithm
-from data_processing import *
-
-import sys
+from data_processing import process_data
 
 # initialize empty collection
-summary = {}
-
-print 'reading in data...'
-
-# read data from csv
-with open('../resources/train.csv', 'rb') as csvfile:
-	reader = csv.reader(csvfile, delimiter=',')
-	for i,record in enumerate(reader):
-		if i == 0:
-			keys = record
-			continue
-		elif i == 501:
-			break
-		summary[i] = {
-			'population': reduce((lambda x,y: int(x)+int(y)), record[402:]),
-			'delta': record[1],
-			'cells': {}
-		}
-		print str(100 *(i/500.0)) + '%'
-		for j,cell in enumerate(keys[402:]):
-			neighbors = get_neighbors(int(cell[5:]), record[401:])
-			density = get_quadrant_densities(record[401:])
-			summary[i]['cells'][j+1] = {
-				'status': record[j+2],
-				'outcome': record[j+398],
-				'neighbors': len(neighbors[0]),
-				'living_neighbors': len(neighbors[1]),
-				'quadrant': get_quadrant(int(cell[5:])),
-				'quadrant_density': density[get_quadrant(int(cell[5:])) - 1],
-				'nearest_edge': get_nearest_edge(int(cell[5:]))
-			}
-			if record[j+2] == '1':
-				summary[i]['cells'][j+1]['size'] = get_size(int(cell[5:]), record[401:])
-			else:
-				summary[i]['cells'][j+1]['size'] = 0
+summary = process_data('../resources/train.csv', 300, True)
 
 # create GA instance
 ga = GeneticAlgorithm()
@@ -53,7 +16,7 @@ fc = FitnessCalc(summary)
 #create initial population
 ga.create_initial_population()
 
-for generations in xrange(1,21):
+for generations in xrange(1,11):
 	max_fitness = 0.0
 	print 'generation ' + str(generations)
 	print 'max fitness: ' + str(max_fitness)
