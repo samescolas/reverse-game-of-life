33641986a1feeddb162a3bf51106259d747398b0
Sam <samescolas@gmail.com>
2017-07-24 20:07:08 +0000

    Working on using scikit-learn to test models.

--- analyze.py
+++ analyze.py
@@ -8,7 +8,7 @@ from sklearn import model_selection, preprocessing
 import xgboost as xgb
 from data_processing import get_neighbors
 
-num_examples = 10
+num_examples = 25
 
 pd.options.mode.chained_assignment = None
 
@@ -20,25 +20,32 @@ start = ['start.' + str(x) for x in xrange(1,401)]
 data = train_df[stop].stack()
 outcomes = train_df[start].stack()
 deltas = train_df['delta'].values
+types = ([1]+[2]*18+[1]) + ([2]+[3]*18+[2])*18 + ([1]+[2]*18+[1])
+
+cols = ['board', 'delta', 'position', 'type', 'alive', 'neighbors', 'population', 'outcome']
 
 processed_data = pd.DataFrame
 
 for board in xrange(num_examples):
-        print data[board].values
-	df = pd.DataFrame([
+	df = [
 		[board]*400,
 		[deltas[board]]*400,
 		range(1,401),
+		[types[x] for x in xrange(400)],
 		list(data[board].values.transpose()),
-		list(outcomes[board].values.transpose()),
                 [get_neighbors(x, data[board].values) for x in xrange(400)],
-		[sum(data[board])]*400
-	])
-
+		[sum(data[board])]*400,
+		list(outcomes[board].values.transpose())
+	]
+	df = [list(l) for l in zip(*df)]
+	df = pd.DataFrame(df, columns=cols)
 	if processed_data.empty:
-		processed_data = df.transpose()
+		processed_data = df
 	else:
-		processed_data = pd.concat([processed_data, df.transpose()])
+		processed_data = pd.concat([processed_data, df], ignore_index=True)
 
-print processed_data.head(20)
-print processed_data.tail(20)
+X = processed_data.drop('outcome', axis=1)
+Y = processed_data['outcome']
+
+print X.head(10)
+print Y.head(10)
