df4cdd515081e22de83e7d26884b35c5651948e4
Sam <samescolas@gmail.com>
2017-07-26 20:36:57 +0000

    Storing computed data to save time.

--- data_processing.py
+++ data_processing.py
@@ -1,3 +1,47 @@
+import pandas as pd
+import pickle
+import os
+
+def process_data(filepath, num_examples=500):
+	if os.path.exists(filepath[0:filepath.rfind('.')] + '.pkl'):
+		return pd.read_pickle(filepath[0:filepath.rfind('.')] + '.pkl')
+	train_df = pd.read_csv(filepath, nrows=num_examples)
+
+	stop = ['stop.' + str(x) for x in xrange(1,401)]
+	start = ['start.' + str(x) for x in xrange(1,401)]
+	
+	data = train_df[stop].stack()
+	outcomes = train_df[start].stack()
+	deltas = train_df['delta'].values
+	types = ([1]+[2]*18+[1]) + ([2]+[3]*18+[2])*18 + ([1]+[2]*18+[1])
+	
+	cols = ['board', 'delta', 'position', 'size', 'type', 'alive', 'neighbors', 'population', 'outcome']
+	
+	processed_data = pd.DataFrame
+	
+	for board in xrange(num_examples):
+		print str(100 * (float(board)/num_examples)) + '%'
+		df = [
+			[board]*400,
+			[deltas[board]]*400,
+			range(1,401),
+			[get_size(x, data[board].values) for x in xrange(400)],
+			[types[x] for x in xrange(400)],
+			list(data[board].values.transpose()),
+	                [get_neighbors(x, data[board].values) for x in xrange(400)],
+			[sum(data[board])]*400,
+			list(outcomes[board].values.transpose())
+		]
+		df = [list(l) for l in zip(*df)]
+		df = pd.DataFrame(df, columns=cols)
+		if processed_data.empty:
+			processed_data = df
+		else:
+			processed_data = pd.concat([processed_data, df], ignore_index=True)
+		processed_data.to_pickle(filepath[0:filepath.rfind('.')] + '.pkl')
+	return processed_data
+
+
 def	get_nghbs(cell, example):
         (l,r,t,b) = (cell-1,cell+1,cell-20,cell+20)
         (tl,tr,bl,br) = (t-1,t+1,b-1,b+1)
